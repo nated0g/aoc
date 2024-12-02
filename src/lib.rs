01d880d6c0ec3920b9c33bd60aceceaef6d89c9e
Nate Usher <nathaniel.usher@stedi.com>
2024-12-01 23:01:38 -0800

    y2024 d02

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub mod solutions {
     }
     pub mod y2024 {
         pub mod day01;
+        pub mod day02;
         // Add more days here
     }
     // Add more years here as needed
@@ -28,22 +29,16 @@ pub fn run_solution((year, day, part): (u32, u32, u32), input: &str) -> String {
         2022 => match (day, part) {
             (1, 1) => y2022::day01::part1(input),
             (1, 2) => y2022::day01::part2(input),
-
             (2, 1) => y2022::day02::part1(input),
             (2, 2) => y2022::day02::part2(input),
-
             (3, 1) => y2022::day03::part1(input),
             (3, 2) => y2022::day03::part2(input),
-
             (4, 1) => y2022::day04::part1(input),
             (4, 2) => y2022::day04::part2(input),
-
             (5, 1) => y2022::day05::part1(input),
             (5, 2) => y2022::day05::part2(input),
-
             (6, 1) => y2022::day06::part1(input),
             (6, 2) => y2022::day06::part2(input),
-
             (7, 1) => y2022::day07::part1(input),
             (7, 2) => y2022::day07::part2(input),
             _ => "Not implemented yet".to_string(),
@@ -51,13 +46,10 @@ pub fn run_solution((year, day, part): (u32, u32, u32), input: &str) -> String {
         2023 => match (day, part) {
             (1, 1) => y2023::day01::part1(input),
             (1, 2) => y2023::day01::part2(input),
-
             (2, 1) => y2023::day02::part1(input),
             (2, 2) => y2023::day02::part2(input),
-
             (3, 1) => y2023::day03::part1(input),
             (3, 2) => y2023::day03::part2(input),
-
             (4, 1) => y2023::day04::part1(input),
             (4, 2) => y2023::day04::part2(input),
             _ => "Not implemented yet".to_string(),
@@ -65,6 +57,8 @@ pub fn run_solution((year, day, part): (u32, u32, u32), input: &str) -> String {
         2024 => match (day, part) {
             (1, 1) => y2024::day01::part1(input),
             (1, 2) => y2024::day01::part2(input),
+            (2, 1) => y2024::day02::part1(input),
+            (2, 2) => y2024::day02::part2(input),
             _ => "Not implemented yet".to_string(),
         },
         _ => "Not implemented yet".to_string(),
---
+++ src/solutions/y2024/day02.rs
@@ -0,0 +1,92 @@
+use std::u32;
+
+fn safe1(nums: &[i32]) -> u32 {
+    let ascending = nums[0] < nums[1];
+
+    let mut prev = nums[0];
+    for &n in &nums[1..] {
+        let diff = (prev - n).abs();
+
+        let no_diff = diff < 1;
+        let too_big_gap = diff > 3;
+        let wrong_direction = (ascending && prev > n) || (!ascending && prev < n);
+
+        if no_diff || too_big_gap || wrong_direction {
+            return 0;
+        }
+        prev = n;
+    }
+    1
+}
+
+fn safe2(nums: &[i32]) -> u32 {
+    if safe1(nums) == 1 {
+        return 1;
+    }
+
+    for skip_idx in 0..nums.len() {
+        let subset: Vec<i32> = nums
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| *i != skip_idx)
+            .map(|(_, &n)| n)
+            .collect();
+
+        if safe1(&subset) == 1 {
+            return 1;
+        }
+    }
+    0
+}
+
+pub fn part1(input: &str) -> String {
+    let sum: u32 = input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let nums: Vec<i32> = line
+                .split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect();
+            safe1(&nums)
+        })
+        .sum();
+    format!("{}", sum)
+}
+
+pub fn part2(input: &str) -> String {
+    let sum: u32 = input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let nums: Vec<i32> = line
+                .split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect();
+            safe2(&nums)
+        })
+        .sum();
+    format!("{}", sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = r#"7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9"#;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT), "2");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), "4");
+    }
+}
